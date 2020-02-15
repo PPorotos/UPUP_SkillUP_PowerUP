e32d59fa3e9b8bdbb2859026ae8e2a2859d669ea
PPorotos <kjh96682@gmail.com>
2020-02-15 22:56:09 +0900

    폴더 수정

---
+++ Step_3/Step3_2438.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int InputNum();
+void PrintStar(int count);
+
+void main() {
+
+	int count = 0;
+
+	count = InputNum();
+	PrintStar(count);
+
+	printf("\n");
+}
+
+int InputNum() {
+	int count = 0;
+
+	do {
+		scanf_s("%d", &count);
+	} while (count < 1 && count > 100);
+
+	return count;
+}
+
+void PrintStar(int count) 
+{
+	int i, j;
+	for (i = 0; i < count; i++) 
+	{
+		for (j = 0; j <= i; j++) 
+			printf("*");
+		
+		printf("\n");
+	}
+}
