2a4fb103a4e8f3a9e6dda26640e02f9d56c444ca
PPorotos <kjh96682@gmail.com>
2020-02-18 21:45:28 +0900

    20200218_algo_step_3

--- Step_3/Step3_10871.c
+++ Step_3/Step3_10871.c
@@ -2,6 +2,8 @@
 #include <Windows.h>
 #include <time.h>
 
+int* RandomNumber(int* arr, int count);
+
 void main() {
 
 	srand(time(0));
@@ -9,10 +11,38 @@ void main() {
 	int N, X, i;
 	int* arr;
 	
-	N = rand() % 11;
-	X = rand() % 11;
-
+	N = rand() % 11 + 1;
+	X = rand() % N + 2;
 	arr = (int*)malloc(sizeof(int) * N);
 
+	for (i = 0; i < N; i++) 
+	{
+		arr[i] = i + 1;
+	}
 
+	arr = RandomNumber(arr, N);
+	
+	for (i = 0; i < N; i++) 
+	{
+		if (X > arr[i])
+			printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+	system("pause");
+}
+
+int* RandomNumber(int* arr,int count) 
+{
+	int i, idx;
+
+	for (i = 0; i < count; i++) 
+	{
+		idx = rand() % count;
+		int temp = arr[i];
+		arr[i] = arr[idx];
+		arr[idx] = temp;
+	}
+
+	return arr;
 }
