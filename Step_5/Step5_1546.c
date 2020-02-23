1ef63b057a1c7779df4192e19c657b261615c5fd
PPorotos <kjh96682@gmail.com>
2020-02-23 22:45:39 +0900

    20200223_algo_step_5

--- Step_5/Step5_1546.c
+++ Step_5/Step5_1546.c
@@ -6,30 +6,29 @@ int main() {
 	
 	srand(time(0));
 
-	int count = 0, i, max, total = 0;
+	int count = rand() % 1001 + 1;
 	int* scores;
+	int i = 0, max = 0;
+	float total = 0;
 
-	count = rand() % 1001 + 1;
 	scores = (int*)malloc(sizeof(int) * count);
-
 
 	for (i = 0; i < count; i++) 
 	{
 		scores[i] = rand() % 101 + 1;
-		
-		if (i == 0)
-			max = scores[i];
-		else if (max < scores[i])
+
+		if (max < scores[i])
 			max = scores[i];
 	}
-
-	for (i = 0; i < count; i++) 
+	for (i = 0; i < count; i++)
 	{
-		total += (scores[i] / max * 100);
+		float temp = 0;
+		temp =scores[i] / (float)max * 100;
+		total += temp;
 	}
 
-	printf("%f\n", (float)total / count);
-	
+	printf("%f\n", total / (float)count);
+
 	system("pause");
 	return 0;
 }
