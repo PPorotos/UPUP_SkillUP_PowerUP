24ed9915c2409d2b7621977540c4ede57acbf465
PPorotos <kjh96682@gmail.com>
2020-02-26 15:32:42 +0900

    20200226_algo_step_5

--- Step_5/Step5_4344.c
+++ Step_5/Step5_4344.c
@@ -1,30 +1,30 @@
-#include<stdio.h>
-#include<Windows.h>
-
-int main()
-{
-	int scores[6] = { 5, 50,50,70,80,100 };
-
-	int i;
-
-	int total = 0;
-	float aver = 0;
-	for (i = 1; i < 6; i++) {
-		total += scores[i];
-	}
-	aver = total / (float)scores[0];
-
-	int count = 0;
-
-	for(i = 1; i<6; i++)
-	{
-		if (aver < scores[i])
-			count++;
-	}
-	aver = count / (float)scores[0] * 100;
-
-	printf("%.3f\n", aver);
-
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<Windows.h>
+//
+//int main()
+//{
+//	int scores[6] = { 5, 50,50,70,80,100 };
+//
+//	int i;
+//
+//	int total = 0;
+//	float aver = 0;
+//	for (i = 1; i < 6; i++) {
+//		total += scores[i];
+//	}
+//	aver = total / (float)scores[0];
+//
+//	int count = 0;
+//
+//	for(i = 1; i<6; i++)
+//	{
+//		if (aver < scores[i])
+//			count++;
+//	}
+//	aver = count / (float)scores[0] * 100;
+//
+//	printf("%.3f\n", aver);
+//
+//	system("pause");
+//	return 0;
+//}
