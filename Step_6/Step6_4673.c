5e7171e4b2fd55579de5cf38d4027bc9c2434666
PPorotos <kjh96682@gmail.com>
2020-03-01 22:16:46 +0900

    20200301_algo_step_6

--- Step_6/Step6_4673.c
+++ Step_6/Step6_4673.c
@@ -1,55 +1,44 @@
 #include<stdio.h>
 #include<Windows.h>
 
-void SelfNum(int num, int* numbers);
+void SelfNum(int num, int* arr);
 
 int main()
 {
-	int* numbers;
+	int* arr;
 	int i;
-	numbers = (int*)malloc(sizeof(int) * 10000);
+
+	arr = (int*)malloc(sizeof(int) * 10000);
 
 	for (i = 0; i < 10000; i++)
-	{
-		numbers[i] = 0;
-	}
+		arr[i] = 0;
 
-	SelfNum(1, numbers);
+	for(i = 0; i<10000; i++)
+		SelfNum(i+1, arr);
 
-	/*for (i = 0; i < 10000; i++)
-	{
-		if (numbers[i] == 0)
+	for (i = 0; i < 10000; i++)
+		if (arr[i] == 0)
 			printf("%d\n", i + 1);
-	}
-*/
+
 	system("pause");
 	return 0;
 }
 
-void SelfNum(int num, int* numbers)
+void SelfNum(int num, int* arr)
 {
-	if (num > 10000)
-		return;
+	int temp = 1;
+	int total = num;
 
-	int value = 0, total = 0, count = 1;
-
-	value = num / count;
-	while (value != 0)
+	while (1)
 	{
-		count *= 10;
-		value = num / count;
+		if (num / temp == 0)
+			break;
+
+		temp *= 10;
+		total += num % temp / (temp / 10);
 	}
 
-	total += num;
-		
-	while (count != 1)
-	{
-		total += (num % count) / (count / 10);
-		count /= 10;
-	}
+	if (total <= 10000)
+		arr[total - 1] = 1;
 
-	printf("%d\n", total);
-
-	numbers[total] = 1;
-	SelfNum(total, numbers);
 }
