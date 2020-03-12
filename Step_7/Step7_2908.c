990e6b8e0e83ede91ea36e24d95af2489549af75
PPorotos <kjh96682@gmail.com>
2020-03-12 12:21:20 +0900

    step_7풀이중

--- Step_7/Step7_2908.c
+++ Step_7/Step7_2908.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
 #include<Windows.h>
 
-int GetNumber(char num1[], char num2[]);
+int GetNumber(char number[]);
 
 void Answer2908()
 {
-	char num1[] = "123";
-	char num2[] = "345";
+	char number[] = "123 345";
 
-	GetNumber(num1, num2);
+	GetNumber(number);
 }
 
 
-int GetNumber(char num1[], char num2[])
+int GetNumber(char number[])
 {
 	int i;
+	char num1[10];
+	char num2[10];
 
+	for (i = 0; number != '\0'; i++)
+	{
+		if (i < 3)
+			num1[i] = number[i];
+		else if (i > 3)
+			num2[i] = number[i];
+	}
+
+	printf("%s\n", num1);
+	printf("%s\n", num2);
 }
