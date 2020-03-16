f83858ae3e1a179da5482c0f5de78f54409edf66
PPorotos <kjh96682@gmail.com>
2020-03-16 21:10:29 +0900

    20200316_algo_step_7

--- Step_7/Step7_2908.c
+++ Step_7/Step7_2908.c
@@ -1,17 +1,55 @@
 #include<stdio.h>
 #include<Windows.h>
 
-int GetNumber(char number[]);
+void ChangeStr(char number[]);
+void CheckValue(char number1[], char number2[]);
 
 void Answer2908()
 {
-	char number[] = "123 345";
+	char number1[3] = "654";
+	char number2[3] = "341";
 
-	GetNumber(number);
+	char* changeNum1;
+	char* changeNum2;
+
+	changeNum1 = (char*)malloc(sizeof(char) * 3);
+	changeNum2 = (char*)malloc(sizeof(char) * 3);
+
+	ChangeStr(number1, changeNum1);
+	ChangeStr(number2, changeNum2);
+
+	CheckValue(changeNum1, changeNum2);
 }
 
-
-int GetNumber(char number[])
+void ChangeStr(char number[], char* changeNum)
 {
-	
+	int i, index = 2;
+
+	for (i = 0; i < 3; i++)
+	{
+		changeNum[i] = number[index];
+		index--;
+	}
+	changeNum[i] = '\0';
 }
+
+void CheckValue(char number1[], char number2[])
+{
+	int i;
+
+	for (i = 0; i <strlen(number1); i++)
+	{
+		if (number1[i] > number2[i])
+		{
+			printf("%s\n", number1);
+			break;
+		}
+		else if (number1[i] < number2[i])
+		{
+			printf("%s\n", number2);
+			break;
+		}
+	}
+
+}
+
