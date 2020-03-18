2115474683b84fd64640cdd539a7e00f02163979
PPorotos <kjh96682@gmail.com>
2020-03-18 23:27:51 +0900

    20200318_algo_step_7

--- Step_7/Step7_5622.c
+++ Step_7/Step7_5622.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <Windows.h>
 
+int DialNumber(char phone[]);
+
 void Answer5622()
 {
-	printf("Test");
+	printf("%d\n", DialNumber("UNUCIC"));
 }
 
+int DialNumber(char phone[]) 
+{
+	int times[] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9,10,10,10,10 };
+	int result = 0;
+	int i;
+	for (i = 0; phone[i] != '\0'; i++)
+		result += times[phone[i] - 'A'];
+
+	return result;
+}
