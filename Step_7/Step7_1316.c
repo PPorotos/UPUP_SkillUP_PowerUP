e7a5cd7a5816d5d65209c8992ed707cad9f535d7
PPorotos <kjh96682@gmail.com>
2020-03-21 17:00:06 +0900

    20200321_algo_step_7

--- Step_7/Step7_1316.c
+++ Step_7/Step7_1316.c
@@ -8,7 +8,20 @@ int CheckStr(char str[]);
 
 void Answer1316()
 {
-	printf("%d\n", CheckStr("happya"));
+	int count = 0;
+	int i;
+	
+	count = scanf_s("%d", &count);
+	char** str = (char**)malloc(sizeof(char*) * count);
+	for (i = 0; i < count; i++)
+	{
+		str[i] = (char*)malloc(sizeof(char) * 10);
+		scanf_s("%s", sizeof(10), str[i]);
+	}
+	for (i = 0; i < count; i++)
+	{
+		printf("%d\n", CheckStr(str[i]));
+	}
 }
 
 int CheckStr(char str[])
