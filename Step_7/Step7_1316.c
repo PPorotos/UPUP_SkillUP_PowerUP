c1ecbe7a1c9267eb17a03e601ff32fb42397ff30
PPorotos <kjh96682@gmail.com>
2020-03-25 22:46:56 +0900

    step_7 변경

--- Step_7/Step7_1316.c
+++ Step_7/Step7_1316.c
@@ -5,13 +5,44 @@
 	입력받은 만큼 체크해서 카운트 할수있게 수정
 */
 int CheckStr(char str[]);
+void SetStr(char* str, char temp[]);
 
 void Answer1316()
 {
+	char** str;
+	int count = 0, i, j;
+	
+	printf("count = ");
+	scanf_s("%d", &count);
+	str = (char**)malloc(sizeof(char*) * count);
 
-	CheckStr("hxy");
-	CheckStr("siiiisss");
-	CheckStr("sldkfj");
+	for (i = 0; i < count; i++)
+		str[i] = (char*)malloc(sizeof(char) * 100);
+
+	for (i = 0; i < count; i++)
+	{ 
+		char temp[100];
+		printf("str[%d] = ", i);
+		scanf_s("%s", temp);
+		SetStr(str[i], temp);
+	}
+		
+
+	for (i = 0; i < count; i++)
+		printf("%d\n", CheckStr(str[i]));
+
+	for (i = 0; i < count; i++)
+		free(str[i]);
+	free(str);
+}
+
+void SetStr(char* str, char temp[])
+{
+	int i;
+	for (i = 0; temp != '\0'; i++)
+		str[i] = temp[i];
+
+	str[i] = '\0';
 }
 
 int CheckStr(char str[])
