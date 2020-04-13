253bf3d79bb4a02a086c1a042d869fce2328e060
PPorotos <kjh96682@gmail.com>
2020-04-13 23:28:34 +0900

    Step_8

--- Step_8/Answer.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void Answer1712();
--- Step_8/Step8_1712.c
+++ Step_8/Step8_1712.c
@@ -1,38 +1,18 @@
 #include <stdio.h>
-#include <Windows.h>
 
-int CalMoney(int a, int b, int c);
-
-void Answer1712()
+int main()
 {
 	int a = 0, b = 0, c = 0;
 	
-	printf("a =");
 	scanf_s("%d", &a);
-	printf("b =");
 	scanf_s("%d", &b);
-	printf("c =");
 	scanf_s("%d", &c);
 
-	printf("%d", CalMoney(a, b, c));
-}
+	if (b >= c)
+		printf("-1\n");
+	else
+		printf("%d\n", a / (c - b) + 1);
 
-int CalMoney(int a, int b, int c)
-{
-	int total = a;
-	int totalPrice = 0;
-	int count = 0;
-	
-	while (1)
-	{
-		count++;
-		
-		total += b;
-		totalPrice += c;
 
-		if (total < totalPrice)
-			break;
-	}
-
-	return count;
+	return 0;
 }
